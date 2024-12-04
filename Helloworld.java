2a525fd9a656502c24490158a610e45474551733
23CSBRAMKUMARB <139440986+23CSBRAMKUMARB@users.noreply.github.com>
2024-12-04 12:22:13 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,8 +1,8 @@
 public class Helloworld{
     public static void main(String args[])
     {
-        for(int i=1;i<=10;i++){
+        
         System.out.println("hello world!");
-    }
+    
     }
 }
